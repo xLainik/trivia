edb387257f60c1b012a090d365baecbd1f293f6a
Erik <erikmuskus@gmail.com>
2024-08-09 22:34:41 -0400

    chat io

--- server/index.js
+++ server/index.js
@@ -3,14 +3,33 @@ import logger from 'morgan'
 
 const port = process.env.PORT ?? 3000
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+	console.log('a user has connected!')
+
+	socket.on('disconnect', () => {
+		console.log('a user has disconnected')
+	})
+
+	socket.on('chat message', (msg) => {
+		// broadcat the chat message to all users
+		io.emit('chat message', msg)
+	})
+})
 
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
+	//send the file index.html to the client
 	res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () =>{
+server.listen(port, () =>{
 	console.log(`Server running on port ${port}`)
 })
