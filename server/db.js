0f206e5782fcb68964197dc031ea605a2c2e398e
Erik <erikmuskus@gmail.com>
2024-08-18 22:45:30 -0400

    styling

--- server/db.js
+++ server/db.js
@@ -40,7 +40,51 @@ export async function createRoom() {
   db_connection.query("USE triviadb;")
   const hash = generateHash(5)
   const room_name = 'room_' + hash
-  const [result] = await db_connection.query(`CREATE TABLE ${room_name} (token VARCHAR(30) PRIMARY KEY, username TEXT(30), score INT);`);
+  const [result] = await db_connection.query(`CREATE TABLE ${room_name} (token VARCHAR(30) PRIMARY KEY, username TEXT(30), score INT, response BOOLEAN);`);
 
   return hash;
+}
+
+export async function checkRoom(hash) {
+  db_connection.query("USE triviadb;")
+  const [result] = await db_connection.query(`SHOW TABLES LIKE 'room_${hash}';`)
+
+  return !result[0]
+}
+
+export async function enterRoom(hash, username) {
+  db_connection.query("USE triviadb;")
+  const [result] = await db_connection.query(`INSERT INTO room_${hash} (token, username, score, response)
+    VALUES ('${username}', '${username}', 0, 0);`)
+
+  return !result[0]
+}
+
+export async function checkUser(hash, username) {
+  db_connection.query("USE triviadb;")
+  const [result] = await db_connection.query(`SELECT * FROM room_${hash} WHERE token='${username}';`)
+
+  return !result[0] == false
+}
+
+export async function updateUser(action, hash, username) {
+  db_connection.query("USE triviadb;")
+  if (action == 'submit score') {
+    const [result] = await db_connection.query(`UPDATE room_${hash} SET score=score+10 WHERE token='${username}';`) 
+  } else if (action == 'check response') {
+    const [result] = await db_connection.query(`SELECT response FROM room_${hash} WHERE token='${username}';`) 
+    return result[0]
+  } else if (action == 'submit response') {
+    const [result] = await db_connection.query(`UPDATE room_${hash} SET response=1 WHERE token='${username}';`) 
+  } else if (action == 'cancel response') {
+    const [result] = await db_connection.query(`UPDATE room_${hash} SET response=1;`) 
+  } else if (action == 'reset response') {
+    const [result] = await db_connection.query(`UPDATE room_${hash} SET response=0;`) 
+  } else if (action == 'check all scores') {
+    const [result] = await db_connection.query(`SELECT username, score FROM room_${hash};`)
+    return result
+  } else if (action == 'remove') {
+    const [result] = await db_connection.query(`DELETE FROM room_${hash} WHERE token='${username}';`) 
+    return result[0]
+  }
 }
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = 3000
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 
-import { getQuestion, createRoom } from './db.js'
+import { getQuestion, createRoom, checkRoom, enterRoom, checkUser, updateUser } from './db.js'
 
 const server = createServer(app)
 const io = new Server(server, {
@@ -21,32 +21,50 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+app.use(express.static('public'));
+
 
 io.on('connection', (socket) => {
-	console.log('a user has connected!')
+
+	console.log(`Socket ${socket.id} has connected!`)
 
 	socket.on('disconnect', () => {
-		console.log('a user has disconnected')
+		console.log(`Socket ${socket.id} has disconnected...`)
+		io.emit('client disconnect')
 	})
 
-	socket.on('question request', (id) => {
+	socket.on('question request', (hash, id) => {
 		console.log(`requesting question ${id}`)
 		app.get(`/question/${id}`)
-		io.emit('question request', id)
+		const response_promise = updateUser('reset response', hash)
+		response_promise.then(io.emit('question request', id))
+		
+		setTimeout(() => {
+			const response_promise = updateUser('cancel response', hash)
+			response_promise.then(io.emit('question timeout'))
+		}, 10000);
+
+		setTimeout(() => {
+			//io.emit('question timeout')
+			io.emit('new question')
+		}, 15000);
 	})
 
-	socket.on('room create', () => {
-		createRoom()
-		.then(function (result) {
-			console.log('room created', result);
-			//app.get(`/lobby/${result}`)
-		})
+	socket.on('room create', (arg, callback) => {
+		const hash_promise = createRoom()
+		hash_promise.then((hash) => {console.log('room created', hash)})
+		hash_promise.then((hash) => {callback(hash)})
 	})
 
-	socket.on('room enter', (hash) => {
-		console.log(`enter room ${hash}`)
-		app.get(`/lobby/${hash}`)
+	socket.on('refresh lobby', (hash) => {
+		console.log('refresh lobby', hash);
+		const response_promise = updateUser('check all scores', hash)
+		response_promise.then((response) => {io.emit('update lobby', response)})
 	})
+
 })
 
 app.use(logger('dev'))
@@ -56,12 +74,54 @@ app.get('/', (req, res) => {
 	res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.get('/lobby/:hash', (req, res) => {
+app.get('/lobby/:hash', async (req, res) => {
 	//enter a room
 	const hash = req.params.hash
-	console.log('entering', hash)
-	res.sendFile(process.cwd() + '/client/lobby.html')
-	res.redirect(`/lobby/${hash}`)
+	const checking = await checkRoom(hash)
+
+	if (!checking == true) {
+		console.log('entering', hash)
+		res.sendFile(process.cwd() + '/client/lobby.html')
+	} else {
+		res.send('<h1>Error: El lobby no existe</h1>')
+	}
+})
+
+app.get('/enterlobby/:hash/:username', async (req, res) => {
+	const hash = req.params.hash
+	const username = req.params.username
+	console.log('checking', hash, username)
+	const check_response = await checkRoom(hash)
+	const checking = await checkUser(hash, username)
+	if (!check_response == true) {
+		if (checking == false) {
+			//The user does not exist
+			const enter_response = await enterRoom(hash, username)
+		}
+	}
+	res.json({exist: checking})
+})
+
+app.post('/answer', async (req, res) => {
+	const hash = req.body.hash
+	const username = req.body.username
+	const answer = req.body.answer
+
+	const player_response = await updateUser('check response', hash, username)
+	console.log(player_response.response);
+	//Check if correct answer
+	if (player_response.response == 0) {
+		if (answer == 1) {
+			console.log("CORRECT!")
+			updateUser('submit score', hash, username)
+		} else {console.log("INCORRECT...", answer)}
+		updateUser('submit response', hash, username)
+	}
+	res.json(player_response)
+})
+
+app.get('/lobby/my_styles.css', async (req, res) => {
+	res.send(process.cwd() + '/public/my_styles.css')
 })
 
 app.get('/question/:id', async (req, res) => {
